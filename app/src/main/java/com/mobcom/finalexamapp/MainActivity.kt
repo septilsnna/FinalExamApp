994b9c57fd3a143b4e5982cd1706366f49b58388
septilsnna <43660194+septilsnna@users.noreply.github.com>
2021-01-13 16:09:08 +0700

    final commit

--- app/src/main/java/com/mobcom/finalexamapp/MainActivity.kt
+++ app/src/main/java/com/mobcom/finalexamapp/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +21,22 @@ class MainActivity : AppCompatActivity() {
         val tv_result = findViewById<TextView>(R.id.tv_result)
 
         button.setOnClickListener {
-            tv_result.text = et_url.text.toString()
+            loading(et_url.text.toString(), tv_result)
         }
     }
 
+    private fun loading(url: String, tv_result: TextView) {
+        val queue = Volley.newRequestQueue(this)
 
+        val stringRequest = StringRequest(Request.Method.GET, "http://$url",
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    tv_result.text = "Response is: ${response.substring(0, 500)}"
+                },
+                Response.ErrorListener { tv_result.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+    }
 }
