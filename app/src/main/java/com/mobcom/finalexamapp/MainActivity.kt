cfd7ac9db648578eff1c70b1caa1bef7dbec410e
septilsnna <43660194+septilsnna@users.noreply.github.com>
2021-01-13 12:57:12 +0700

    first commit

---
+++ app/src/main/java/com/mobcom/finalexamapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobcom.finalexamapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
