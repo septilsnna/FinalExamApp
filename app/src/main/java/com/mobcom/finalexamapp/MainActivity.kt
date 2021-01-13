ad9f199d8f6458551021f1d473ae8541f772d55a
septilsnna <43660194+septilsnna@users.noreply.github.com>
2021-01-13 15:22:37 +0700

    commit

--- app/src/main/java/com/mobcom/finalexamapp/MainActivity.kt
+++ app/src/main/java/com/mobcom/finalexamapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.mobcom.finalexamapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val et_url = findViewById<EditText>(R.id.url_input)
+        val button = findViewById<Button>(R.id.get_source)
+        val tv_result = findViewById<TextView>(R.id.tv_result)
+
+        button.setOnClickListener {
+            tv_result.text = et_url.text.toString()
+        }
     }
+
+
 }
